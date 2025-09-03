e3827c8fe5920b6d9adcb7e44dd1c903c126fc6a
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2025-09-03 07:06:17 +0100

    fix: guarantor step 4 docuent reading undefined property name

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -40,7 +40,7 @@ export interface LoadingButtonProps extends ButtonProps {
   loading?: boolean;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+const Button = React.forwardRef<HTMLButtonElement, LoadingButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
     return (
